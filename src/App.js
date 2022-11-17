63a0ee1647ac0b9b2a56fa8d6a7ba6621cf1fd12
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-17 23:58:56 +0900

    chore : App.js, index.html 수정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from "react-router-dom";
+import List from './pages/List';
+import Login from './pages/Login';
+import Data from './component/Data';
+import Detail from './pages/Detail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Data />
+      <Routes>
+        <Route path='login' element={<Login />} />
+        <Route path='list' element={<List />}/>
+        <Route path="/detail/:contentId" element={<Detail />} />
+      </Routes>
+    </>
   );
 }
 
