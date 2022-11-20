3a6911ad8ec45481e95e6efcf8d8c68638ef677a
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-20 18:04:04 +0900

    style : App.js 라우터 수정

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from "react-router-dom";
 import List from './pages/List';
 import Login from './pages/Login';
-import Data from './component/Data';
 import Detail from './pages/Detail';
+import Main from './pages/Main';
+import SubMain from './pages/SubMain';
 
 function App() {
   return (
     <>
-      <Data />
       <Routes>
-        <Route path='login' element={<Login />} />
-        <Route path='list' element={<List />}/>
-        <Route path="/detail/:contentId" element={<Detail />} />
+        <Route path='/' element={<Main />} />
+        <Route path='/submain/:region' element={<SubMain />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/list/:region/:type' element={<List />}/>
+        <Route path="/detail/:region/:contentId" element={<Detail />} />
       </Routes>
     </>
   );
