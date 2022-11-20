35723118616f3eadd04aaa700c7728af626468ad
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-20 18:04:52 +0900

    feat : 데이터를 서버에 저장하는 기능

--- src/component/Data.jsx
+++ src/component/Data.jsx
@@ -5,20 +5,30 @@ export default function Data() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get('https://apis.data.go.kr/B551011/KorService/areaBasedList?serviceKey=rfaoGpiapHFqOcUT6bqfERRxy1WVxzOdOpEC3ChyAFPEfONdSMdRVNETTJKRhqTbPuZ2krpG2mQJMXDbyG74RA%3D%3D&numOfRows=100&pageNo=1&MobileOS=ETC&MobileApp=TripLog&_type=json&listYN=Y&arrange=B&contentTypeId=12&areaCode=1')
-    .then((response) => {
-      setData(response.data.response.body.items.item)
-    })
+    axios.get('https://apis.data.go.kr/B551011/KorService/areaBasedList?serviceKey=rfaoGpiapHFqOcUT6bqfERRxy1WVxzOdOpEC3ChyAFPEfONdSMdRVNETTJKRhqTbPuZ2krpG2mQJMXDbyG74RA%3D%3D&numOfRows=989&pageNo=1&MobileOS=ETC&MobileApp=TripLog&_type=json&listYN=Y&arrange=B&contentTypeId=39&areaCode=39')
+      .then((response) => {
+        setData(response.data.response.body.items.item)
+      })
   }, [])
 
   function apiData() {
-    axios.post('http://localhost:4000/test', data)
-    .then((response) => {
-      console.log('성공')
-    })
+    axios.post('http://localhost:4000/test/1', data)
+      .then((response) => {
+        console.log('성공')
+      })
   }
-  
+
+  function apiData1() {
+    axios.post('http://localhost:4000/test/2', data)
+      .then((response) => {
+        console.log('성공')
+      })
+  }
+  console.log(data)
   return (
-    <button onClick={apiData}>데이터전송</button>
+    <>
+      <button onClick={apiData}>전체데이터전송</button>
+      <button onClick={apiData1}>기타데이터전송</button>
+    </>
   )
 }
