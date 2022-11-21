59d9525c7e7bf0a457762b2e2058c121b5d90e40
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-22 02:33:22 +0900

    refactor : 코드 위치 수정

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,7 +1,7 @@
-import axios from "axios"
 import { useEffect, useState } from "react"
 import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+import axios from "axios"
 import ListDetail from "./ListDetail";
 
 export default function List() {
