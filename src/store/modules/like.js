01a75cd8d8ea32401252e996c77c601d9c8893a7
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-30 23:44:42 +0900

    feat : 좋아요 리덕스 추가

--- src/store/modules/like.js
+++ src/store/modules/like.js
@@ -1,26 +1,26 @@
 // 액션 타입
-const REVIEW_UPDATE = 'detail/REVIEW_UPDATE';
+const LIKE_UPDATE = 'LIKE_UPDATE';
 
 // 액션 생성 함수
-export function reviewUpdate() {
+export function likeUpdate() {
   return {
-    type: REVIEW_UPDATE,
+    type: LIKE_UPDATE,
   };
 };
 
 // 초기 상태 설정
 const initState = {
-  reviewUpdate: false,
+  likeUpdate: false,
 };
 
 // 리듀서
-export default function detail(state = initState, action) {
+export default function like(state = initState, action) {
   switch (action.type) {
-    case REVIEW_UPDATE:
+    case LIKE_UPDATE:
       return {
         ...state,
         // 업데이트 여부에서 데이터 값만 변화하면 되므로 NOT 을 붙여서 처리
-        reviewUpdate: !state.reviewUpdate,
+        likeUpdate: !state.likeUpdate,
       };
     default:
       return state;
